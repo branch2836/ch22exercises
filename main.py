f6cdb0baaaa117304cba7b764d61c54dfccedb98
Branch, Christopher <Christopher_Branch@comcast.com>
2023-01-04 15:09:40 -0500

    22.6

--- main.py
+++ main.py
@@ -7,12 +7,22 @@ app.config['DEBUG'] = True
 def checkbox_form():
     pizza_toppings = ["pepperoni", "sausage", "mushrooms", "onions", "green peppers"]
     input = "checkbox"
+    tab_title = 'Template Logic'
+    page_title = 'Checkbox Logic'
     
     if request.method == 'POST':
         choices = request.form.getlist('toppings')
     else:
         choices = []
-    return render_template('checkbox_form.html', pizza_toppings=pizza_toppings,choices=choices, input=input )
+    return render_template('checkbox_form.html', pizza_toppings=pizza_toppings,choices=choices, input=input, \
+        tab_title=tab_title, page_title=page_title )
+
+@app.route('/second', methods=['GET', 'POST'])
+def second_page():
+    tab_title= 'Template Logic'
+    page_title='Second Page'
+
+    return render_template('second.html', tab_title=tab_title, page_title=page_title)
 
 if __name__ == '__main__':
     app.run()
