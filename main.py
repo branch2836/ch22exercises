f46d679dd6fe35c678fa6e3bd61f6be8d052c8c8
Branch, Christopher <Christopher_Branch@comcast.com>
2023-01-04 12:43:21 -0500

    22.5

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ app.config['DEBUG'] = True
 @app.route('/', methods=['GET', 'POST'])
 def checkbox_form():
     pizza_toppings = ["pepperoni", "sausage", "mushrooms", "onions", "green peppers"]
-    input = "radio"
-    if request.method == "POST":
-        input = request.form['input-type']
-    return render_template('checkbox_form.html', pizza_toppings=pizza_toppings,input=input )
+    input = "checkbox"
+    
+    if request.method == 'POST':
+        choices = request.form.getlist('toppings')
+    else:
+        choices = []
+    return render_template('checkbox_form.html', pizza_toppings=pizza_toppings,choices=choices, input=input )
 
 if __name__ == '__main__':
     app.run()
