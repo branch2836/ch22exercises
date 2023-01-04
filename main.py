263cd4472d4420c07b7e1d258332314636bbcb86
Branch, Christopher <Christopher_Branch@comcast.com>
2023-01-04 15:53:43 -0500

    22.7

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+navigation = {
+    'Pizza Toppings Form': '/',
+    'Second Page': '/second'
+}
+
 @app.route('/', methods=['GET', 'POST'])
 def checkbox_form():
     pizza_toppings = ["pepperoni", "sausage", "mushrooms", "onions", "green peppers"]
@@ -15,14 +20,14 @@ def checkbox_form():
     else:
         choices = []
     return render_template('checkbox_form.html', pizza_toppings=pizza_toppings,choices=choices, input=input, \
-        tab_title=tab_title, page_title=page_title )
+        tab_title=tab_title, page_title=page_title , navigation = navigation)
 
 @app.route('/second', methods=['GET', 'POST'])
 def second_page():
     tab_title= 'Template Logic'
     page_title='Second Page'
 
-    return render_template('second.html', tab_title=tab_title, page_title=page_title)
+    return render_template('second.html', tab_title=tab_title, page_title=page_title , navigation = navigation)
 
 if __name__ == '__main__':
     app.run()
